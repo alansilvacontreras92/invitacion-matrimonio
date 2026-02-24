73c1ee7b9110a5d79f78c1935a55bab3b21ea309
alansilvacontreras92 <alan.silva.contreras92@gmail.com>
2026-02-24 19:54:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Cuenta regresiva
+const weddingDate = new Date("Oct 25, 2025 18:00:00").getTime();
+
+setInterval(function() {
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+}, 1000);
